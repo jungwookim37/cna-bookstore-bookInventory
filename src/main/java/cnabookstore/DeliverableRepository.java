8f93a311277942ca98237c03e1d2c9cfa83a4887
root <crossorbit@gmail.com>
2020-09-07 09:20:55 +0000

    20200907_2

--- src/main/java/cnabookstore/DeliverableRepository.java
+++ src/main/java/cnabookstore/DeliverableRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface DeliverableRepository extends PagingAndSortingRepository<Deliverable, Long>{
 
     Optional<List<Deliverable>> findByBookIdAndStatusOrderByOrderIdAsc(Long bookId, String stockLacked);
+    Optional<Deliverable> findByOrderId(Long orderId);
 }
